17e9bb398ff886f4096f95f51805744128f49190
danishjavedcodes <u2021134@giki.edu.pk>
2025-03-19 16:28:48 +0500

    fixed path

--- fitnessbase/__init__.py
+++ fitnessbase/__init__.py
@@ -1,8 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from fitnessbase.config import Config  # Ensure this import is correct
-from ../ import config
+from flask_login import login_manager
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess-string'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        "postgresql://postgres:wmepPSOcLBehjMxScBFsxCbnPVEfcUIQ@caboose.proxy.rlwy.net:34585/railway"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Flask-Login configuration
+    SESSION_PROTECTION = 'strong'
+    
+    # Bcrypt configuration
+    BCRYPT_LOG_ROUNDS = 12
+
 
 db = SQLAlchemy()
 login_manager = LoginManager()
